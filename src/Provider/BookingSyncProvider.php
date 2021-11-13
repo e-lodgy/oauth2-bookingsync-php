d2319bdcf608d9ac7c3b42960afe1641cf258a33
benjaminmal <bbenben778@gmail.com>
2021-11-13 17:26:30 +0100

    Remove duplicated with interface comments

--- src/Provider/BookingSyncProvider.php
+++ src/Provider/BookingSyncProvider.php
@@ -21,29 +21,16 @@ class BookingSyncProvider extends AbstractProvider
 
     protected string $version = 'v3';
 
-    /**
-     * Returns the base URL for authorizing a client.
-     *
-     * Eg. https://oauth.service.com/authorize
-     */
     public function getBaseAuthorizationUrl(): string
     {
         return 'https://www.bookingsync.com/oauth/authorize';
     }
 
-    /**
-     * Returns the base URL for requesting an access token.
-     *
-     * Eg. https://oauth.service.com/token
-     */
     public function getBaseAccessTokenUrl(array $params = []): string
     {
         return 'https://www.bookingsync.com/oauth/token';
     }
 
-    /**
-     * Returns the URL for requesting the resource owner's details.
-     */
     public function getResourceOwnerDetailsUrl(AccessTokenInterface $token): string
     {
         $id = $token instanceof ResourceOwnerAccessTokenInterface ? $token->getResourceOwnerId() : null;
@@ -51,24 +38,11 @@ class BookingSyncProvider extends AbstractProvider
         return 'https://www.bookingsync.com/api/' . $this->version . '/accounts/' . $id;
     }
 
-    /**
-     * Returns the default scopes used by this provider.
-     *
-     * This should only be the scopes that are required to request the details
-     * of the resource owner, rather than all the available scopes.
-     */
     protected function getDefaultScopes(): array
     {
         return ['public'];
     }
 
-    /**
-     * Checks a provider response for errors.
-     *
-     * @param array|string $data Parsed response data
-     *
-     * @throws IdentityProviderException
-     */
     protected function checkResponse(ResponseInterface $response, $data): void
     {
         if ($response->getStatusCode() >= 400 && ! isset($data['errors'])) {
@@ -86,19 +60,11 @@ class BookingSyncProvider extends AbstractProvider
         }
     }
 
-    /**
-     * Returns the string that should be used to separate scopes when building
-     * the URL for requesting an access token.
-     */
     protected function getScopeSeparator(): string
     {
         return ' ';
     }
 
-    /**
-     * Generates a resource owner object from a successful resource owner
-     * details request.
-     */
     protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface
     {
         if (! array_key_exists('accounts', $response) || empty($response['accounts'])) {
